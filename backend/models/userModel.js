0998a386b8699700b5ab72aeb3819d2625771ea2
Vincent Kipngetich <vinniebluz@icloud.com>
2023-07-05 02:11:05 +0300

    Created a user schema model

---
+++ backend/models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add a email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minLength: [6, "Password must be up to 6 characters"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Please add a Photo"],
+      default: "https://i.ibb.co/3k2BG3T/profile.png",
+    },
+    phone: {
+      type: String,
+      default: "+254",
+    },
+    bio: {
+      type: String,
+      maxLength: [250, "Bio must not be more than 250 Characters"],
+      default: "bio",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
