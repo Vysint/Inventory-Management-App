f853c04ec0e8475a094c9189030961091e947b34
Vincent Kipngetich <vinniebluz@icloud.com>
2023-07-19 15:38:29 +0300

    Added create Product controller function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,5 +4,33 @@ const Product = require("../models/productModel");
 // route   POST /api/products
 // @access Private
 exports.createProduct = async (req, res, next) => {
-  res.send("Product created");
+  const { name, sku, category, quantity, price, description } = req.body;
+
+  // Validation
+  try {
+    if (!name || !category || !quantity || !price || !description) {
+      res.status(400);
+      throw new Error("Please fill in all fields");
+    }
+  } catch (err) {
+    return next(err);
+  }
+
+  // Manage image upload
+
+  // create a product
+  try {
+    const product = await Product.create({
+      user: req.user._id,
+      name,
+      sku,
+      category,
+      quantity,
+      price,
+      description,
+    });
+    res.status(201).json(product);
+  } catch (err) {
+    return next(err);
+  }
 };
