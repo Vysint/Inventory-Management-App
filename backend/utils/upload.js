134b0dd053fe7627d29023813ff857ce2694978d
Vincent Kipngetich <vinniebluz@icloud.com>
2023-07-20 22:55:26 +0300

    Changed the naming of the uploaded files

--- backend/utils/upload.js
+++ backend/utils/upload.js
@@ -6,7 +6,10 @@ const storage = multer.diskStorage({
     cb(null, "uploads");
   },
   filename: function (req, file, cb) {
-    cb(null, Date().toISOString().replace(/:/g, "-")) + file.originalname;
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+    );
   },
 });
 
