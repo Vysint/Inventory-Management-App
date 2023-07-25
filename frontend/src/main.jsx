41cb6dba24e08e7de59fc34a0791486ffc13cf1c
Vincent Kipngetich <vinniebluz@icloud.com>
2023-07-25 19:47:49 +0300

    Set up routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import {
+  createRoutesFromElements,
+  RouterProvider,
+  createBrowserRouter,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
