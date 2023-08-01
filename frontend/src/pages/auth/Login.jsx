9eae51e3508ea24158e4cf057b0aaa5979d61743
Vincent Kipngetich <vinniebluz@icloud.com>
2023-08-02 02:00:48 +0300

    Modified Login Page and added it's CSS Styling

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { BiLogIn } from "react-icons/bi";
+import Card from "../../components/card/Card";
+import { Link } from "react-router-dom";
+import styles from "./auth.module.scss";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <BiLogIn size={35} color="#999" />
+          </div>
+          <h2>Login</h2>
+          <form className="form">
+            <input type="email" placeholder="Email" name="email" required />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              required
+            />
+            <button type="submit" className="--btn --btn-primary --btn-block">
+              Login
+            </button>
+          </form>
+          <Link to="/forgotpassword">Forgot Password</Link>
+          <span className={styles.register}>
+            <Link to="/">Home</Link>
+            <p> &nbsp; Don't have an account? &nbsp;</p>
+            <Link to="/register">Register</Link>
+          </span>
+        </div>
+      </Card>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
