7b3f42ff74e44a695d03bd4d3653b47e41292b22
Vincent Kipngetich <vinniebluz@icloud.com>
2023-08-04 01:34:29 +0300

    Logout user api call

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,4 +1,16 @@
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../services/authService";
+import { SET_LOGIN } from "../../redux/features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logout = async () => {
+    await logoutUser();
+    await dispatch(SET_LOGIN(false));
+    navigate("/");
+  };
   return (
     <div className="--pad header">
       <div className="--flex-between">
@@ -6,7 +18,9 @@ const Header = () => {
           <span className="--fw-thin">Welcome, </span>
           <span className="--color-danger">Vintage</span>
         </h3>
-        <button className="--btn --btn-danger">Logout</button>
+        <button className="--btn --btn-danger" onClick={logout}>
+          Logout
+        </button>
       </div>
       <hr />
     </div>
