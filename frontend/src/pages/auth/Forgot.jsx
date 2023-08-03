a821eb5fdc38894770b0b142ce54acff9c9cdab1
Vincent Kipngetich <vinniebluz@icloud.com>
2023-08-04 02:25:43 +0300

    Forgot Password api call

--- frontend/src/pages/auth/Forgot.jsx
+++ frontend/src/pages/auth/Forgot.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import { AiOutlineMail } from "react-icons/ai";
 import Card from "../../components/card/Card";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { forgotPassword, validateEmail } from "../../services/authService";
 import styles from "./auth.module.scss";
 
 const Forgot = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!email) {
+      return toast.error("Please enter an email");
+    }
+    if (!validateEmail(email)) {
+      return toast.error("Please enter a valid email");
+    }
+    const userInfo = {
+      email,
+    };
+
+    await forgotPassword(userInfo);
+    setEmail("");
+  };
   return (
     <div className={`container ${styles.auth}`}>
       <Card>
@@ -12,8 +32,15 @@ const Forgot = () => {
             <AiOutlineMail size={35} color="#999" />
           </div>
           <h2>Forgot Password</h2>
-          <form className="form">
-            <input type="email" placeholder="Email" name="email" required />
+          <form className="form" onSubmit={handleSubmit}>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
             <button
               type="submit"
               className="--btn --btn-primary --btn-block"
