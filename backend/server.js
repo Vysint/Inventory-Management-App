efbc073e976d76ab49be6e94ebbed169338dac03
Vincent Kipngetich <vinniebluz@icloud.com>
2023-08-04 00:03:22 +0300

    Set up cors() to allow api calls

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
