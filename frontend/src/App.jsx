45f4da66b85ad3314d0081fa0fde7124add36b53
Vincent Kipngetich <vinniebluz@icloud.com>
2023-08-03 22:11:55 +0300

    Set up react-toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import axios from "axios";
+import { ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 axios.defaults.withCredentials = true;
 const App = () => {
   return (
     <>
       <Outlet />
+      <ToastContainer />
     </>
   );
 };
