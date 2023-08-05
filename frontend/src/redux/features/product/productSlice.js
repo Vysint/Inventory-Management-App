5da5f858c02fcf55d9fa6fde3393051d064a10b5
Vincent Kipngetich <vinniebluz@icloud.com>
2023-08-05 12:12:42 +0300

    CreateAsyncTank for product creation

--- frontend/src/redux/features/product/productSlice.js
+++ frontend/src/redux/features/product/productSlice.js
@@ -1,4 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createNewProduct } from "../../../services/productService";
+import { toast } from "react-toastify";
 
 const initialState = {
   product: null,
@@ -9,13 +11,47 @@ const initialState = {
   message: [],
 };
 
+// Create a new product
+const createProduct = createAsyncThunk(
+  "products/create",
+  async (formData, thunk) => {
+    try {
+      return await createNewProduct(formData);
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+      console.log(message);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 const productSlice = createSlice({
   name: "product",
   initialState,
   reducers: {
     CALC_STORE_VALUE: (state, value) => {},
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(createProduct.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(createProduct.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSucccess = true;
+      console.log(action.payload);
+      state.products.push(action.payload);
+      toast.success("Product added successfully");
+    });
+    builder.addCase(createProduct.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.message = action.payload;
+      toast.error(action.payload);
+    });
+  },
 });
 
 export const { CALC_STORE_VALUE } = productSlice.actions;
