3bd88ba92c0452d2025d7a1f65b99e56692883e1
Vincent Kipngetich <vinniebluz@icloud.com>
2023-08-07 13:59:04 +0300

    Create product component

---
+++ frontend/src/pages/addProduct/AddProduct.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ProductForm from "../../components/productForm/ProductForm";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { createProduct } from "../../redux/features/product/productSlice";
+
+const initialState = {
+  name: "",
+  category: "",
+  quantity: "",
+  price: "",
+};
+
+const AddProduct = () => {
+  const [product, setProduct] = useState(initialState);
+  const [productImage, setProductImage] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const [description, setDescription] = useState("");
+
+  const { isLoading } = useSelector((state) => state.product);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { name, category, quantity, price } = product;
+
+  const handleInputChange = (e) => {
+    setProduct((product) => ({ ...product, [e.target.name]: e.target.value }));
+  };
+
+  const handleImageChange = (e) => {
+    setProductImage(e.target.files[0]);
+    setImagePreview(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const generateSKU = (category) => {
+    const letter = category.slice(0, 3).toUpperCase();
+    const number = Date.now();
+    const sku = letter + "-" + number;
+    return sku;
+  };
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("sku", generateSKU(category));
+    formData.append("category", category);
+    formData.append("quantity", quantity);
+    formData.append("price", price);
+    formData.append("description", description);
+    formData.append("image", productImage);
+    console.log(...formData);
+
+    try {
+      await dispatch(createProduct(formData));
+      navigate("/dashboard");
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
+  return (
+    <>
+      <h3 className="--mt">Add New Product</h3>
+      <ProductForm
+        product={product}
+        productImage={productImage}
+        imagePreview={imagePreview}
+        description={description}
+        setDescription={setDescription}
+        handleInputChange={handleInputChange}
+        handleImageChange={handleImageChange}
+        saveProduct={saveProduct}
+      />
+    </>
+  );
+};
+
+export default AddProduct;
