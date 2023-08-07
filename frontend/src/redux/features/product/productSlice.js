f81f41cb5afb976411fbafe4353d5e91cc38340a
Vincent Kipngetich <vinniebluz@icloud.com>
2023-08-08 00:52:45 +0300

    Created a Get All Products function in redux

--- frontend/src/redux/features/product/productSlice.js
+++ frontend/src/redux/features/product/productSlice.js
@@ -1,5 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { createNewProduct } from "../../../services/productService";
+import {
+  createNewProduct,
+  getAllProducts,
+} from "../../../services/productService";
 import { toast } from "react-toastify";
 
 const initialState = {
@@ -14,9 +17,25 @@ const initialState = {
 // Create a new product
 export const createProduct = createAsyncThunk(
   "products/create",
-  async (formData, thunk) => {
+  async (formData, thunkAPI) => {
     try {
       return await createNewProduct(formData);
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+      console.log(message);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+// Get All Products
+export const getProducts = createAsyncThunk(
+  "products/getAll",
+  async (_, thunkAPI) => {
+    try {
+      return await getAllProducts(formData);
     } catch (err) {
       const message =
         (err.response && err.response.data && err.response.data.message) ||
@@ -35,22 +54,37 @@ const productSlice = createSlice({
     CALC_STORE_VALUE: (state, value) => {},
   },
   extraReducers: (builder) => {
-    builder.addCase(createProduct.pending, (state) => {
-      state.isLoading = true;
-    });
-    builder.addCase(createProduct.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.isSucccess = true;
-      console.log(action.payload);
-      state.products.push(action.payload);
-      toast.success("Product added successfully");
-    });
-    builder.addCase(createProduct.rejected, (state, action) => {
-      state.isLoading = false;
-      state.isError = true;
-      state.message = action.payload;
-      toast.error(action.payload);
-    });
+    builder
+      .addCase(createProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSucccess = true;
+        state.products.push(action.payload);
+        toast.success("Product added successfully");
+      })
+      .addCase(createProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        toast.error(action.payload);
+      })
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSucccess = true;
+        state.isError = false;
+        state.products = action.payload;
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        toast.error(action.payload);
+      });
   },
 });
 
